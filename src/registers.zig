c49ce7ddced650c1b6ac0c13ab0364468c9582f5
4zv4l <4zv4l@protonmail.com>
2024-08-07 17:31:15 +0800

    up: add FLAGS register for condition

--- src/registers.zig
+++ src/registers.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 pub const Registers = std.EnumArray(Register, u8);
-pub const Register = enum(u8) { A, B, C, BP, SP, PC };
+pub const Register = enum(u8) { A, B, C, BP, SP, PC, FLAGS };
 
 pub fn fmtRegistersImpl(
     self: Registers,
@@ -11,7 +11,7 @@ pub fn fmtRegistersImpl(
     try writer.print(
         \\A: 0x{x:0>2}        BP: 0x{x:0>2}
         \\B: 0x{x:0>2}        SP: 0x{x:0>2}
-        \\C: 0x{x:0>2}        PC: 0x{x:0>2}
+        \\C: 0x{x:0>2}        PC: 0x{x:0>2}     FLAGS: 0x{x:0>2}
     , .{
         self.get(.A),
         self.get(.BP),
@@ -19,5 +19,6 @@ pub fn fmtRegistersImpl(
         self.get(.SP),
         self.get(.C),
         self.get(.PC),
+        self.get(.FLAGS),
     });
 }
